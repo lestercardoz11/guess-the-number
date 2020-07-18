24047618281a7bda6b1c7f11428fc485f33a21d9
lestercardoz11 <lestercardoz11@gmail.com>
2020-07-18 16:10:49 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <title>Lester Cardoz - Guessing game</title>
+</head>
+<body>
+    <h1>Welcome to my Guessing game</h1>
+<p>
+    <?php
+    if (! isset($_GET['guess'])) {
+        echo "Missing guess parameter";
+    } else if (strlen($_GET['guess']) < 1) {
+        echo "Your guess is too short";
+    } else if (! is_numeric($_GET['guess'])) {
+        echo "Your guess is not a number";
+    } else if ($_GET['guess'] < 57){
+        echo "Your guess is too low";
+    } else if ($_GET['guess'] > 57){
+        echo "Your guess is too high";
+    } else {
+        echo "Congratulations - You are right";
+    }
+    ?>
+</p>
+</body>
